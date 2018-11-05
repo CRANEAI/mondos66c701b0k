085afe60df519799464d206caf38b6190686701e
Elly <elly@mg.crane.ai>
2018-11-05 16:05:20 -0500

    updated client/src/reducers/index refactoring

--- client/src/reducers/index.jsx
+++ client/src/reducers/index.jsx
@@ -1,16 +1,16 @@
 import { combineReducers } from "redux"
 import { reducer as Form } from "redux-form"
 
-import signup from "./reducer_signup"
-import create_account from "./reducer_create_account"
+import payment from "./reducer_payment"
 import payment from "./reducer_payment"
 import live from "./reducer_live"
+import create_account from "./reducer_create_account"
 
 const rootReducer = combineReducers({
-  signup: signup,
-  create_account: create_account,
   payment: payment,
-  live: live
+  payment: payment,
+  live: live,
+  create_account: create_account
 })
 
 export default rootReducer
