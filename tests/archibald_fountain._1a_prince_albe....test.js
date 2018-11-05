f0cc285c81a46486aff83f9e0f9b1da56d39d164
Elly <elly@mg.crane.ai>
2018-11-05 13:54:13 -0500

    added tests/archibald_fountain refactoring

---
+++ tests/archibald_fountain._1a_prince_albe....test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - archibald fountain. 1a prince albe... UNIT TESTS
+ * Application Client
+ * fanyc
+*/
+
+/* the movies view has a notification icon located at the bottom center of the view which on click will trigger the notification() action */
+it("the movies view has a notification icon located at the bottom center of the view which on click will trigger the notification() action", () => {
+  /* ToDo developer needs to complete unit test 
+               the movies view has a notification icon located at the bottom center of the view which on click will trigger the notification() action
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
