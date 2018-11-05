829d33039ea6d8c2a96266f259833fb37a066cea
Elly <elly@mg.crane.ai>
2018-11-05 13:53:48 -0500

    added client/src/reducers/index refactoring

---
+++ client/src/reducers/index.jsx
@@ -0,0 +1,16 @@
+
+        import { combineReducers }                      from 'redux';
+        import { reducer as Form }                      from 'redux-form';
+
+        import movies from './reducer_movies' 
+import archibald_fountain._1a_prince_albe... from './reducer_archibald_fountain._1a_prince_albe...'
+
+        const rootReducer = combineReducers({
+            movies : movies 
+,archibald_fountain._1a_prince_albe... : archibald_fountain._1a_prince_albe... 
+
+        })
+
+        export default rootReducer;
+
+        
